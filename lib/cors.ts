95ad35c2fe02d36dbfd0c53a5fa37b7c7bc6080f
navingrg <grgnabin60@gmail.com>
2025-05-27 09:42:22 +0545

    Add CORS handling utility to manage allowed origins and response headers

---
+++ lib/cors.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://video-recorder-optebc0ka-grgnavins-projects.vercel.app",
+  "https://video-recorder-silk.vercel.app"
+];
+
+export function handleCORS(req: NextRequest) {
+  const origin = req.headers.get("origin") || "";
+  const res = new NextResponse();
+
+  if (allowedOrigins.includes(origin)) {
+    res.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.headers.set("Access-Control-Allow-Credentials", "true");
+
+  return res;
+}
